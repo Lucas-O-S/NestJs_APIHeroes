40881b50bc08db863577badd8dfb0c25c10ce5de
Lucas-O-S <lucasoscomercial@gmail.com>
2024-12-16 14:35:55 -0300

    Ajustes de cadastro de herois

--- src/dados-herois/dados-herois.controller.ts
+++ src/dados-herois/dados-herois.controller.ts
@@ -1,15 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseInterceptors, UploadedFiles, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator } from '@nestjs/common';
 import { DadosHeroisService } from './dados-herois.service';
 import { CreateDadosHeroisDto } from './dto/create-dados-herois.dto';
 import { UpdateDadosHeroisDto } from './dto/update-dados-herois.dto';
 import {Heroes} from '../models/heroes.model';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('dados-herois')
 export class DadosHeroisController {
   constructor(private readonly dadosHeroisService: DadosHeroisService) {}
 
+  @UseInterceptors(FilesInterceptor("imagens"))
   @Post()
-  insere(@Body() createDadosHeroisDto: CreateDadosHeroisDto) {
+  insere(
+    @Body() createDadosHeroisDto: CreateDadosHeroisDto,
+    @UploadedFiles(new ParseFilePipe({
+      validators: [
+        new FileTypeValidator({ fileType: /(image\/jpeg|image\/png)/ }),
+        new MaxFileSizeValidator({maxSize: 1024 * 1000 * 2})
+      ]
+    })) imagens: Express.Multer.File[])
+  {
+    createDadosHeroisDto.image = imagens[0];
+    createDadosHeroisDto.converImage = imagens[1];
+
     return this.dadosHeroisService.create(createDadosHeroisDto);
   }
 
--- src/dados-herois/dto/create-dados-herois.dto.ts
+++ src/dados-herois/dto/create-dados-herois.dto.ts
@@ -1,30 +1,66 @@
-import {IsNumber, IsString} from "class-validator";
+import { Transform } from "class-transformer";
+import {IsDate, IsNotEmpty, IsString, MinDate, MinLength} from "class-validator";
 
 export class CreateDadosHeroisDto {
-    @IsString()
+    @IsString({message: "name deve ser string"})
+    @MinLength(2, {message: "name deve Conter mais de dois caracteres"})
     readonly name: string;
 
-    @IsString()
-    readonly moralidade: string;
-
-    @IsString()
+    @IsString({message: "studio deve ser string"})
+    @MinLength(2, {message: "studio deve Conter mais de dois caracteres"})
+    @IsNotEmpty({message: "studio não pode estar vazio"})
     readonly studio: string;
 
-    @IsNumber()
-    readonly anoLancamento: Number;
 
-    @IsString()
-    readonly image: string;
 
-    @IsString()
-    readonly image_cover: string;
+    @IsString({message: "powerType deve ser string"})
+    @MinLength(2, {message: "powerType deve Conter mais de dois caracteres"})
+    @IsNotEmpty({message: "powerType, não pode estar vazio"})
+    readonly powerType: string;
 
-    @IsString()
-    readonly  equipe: string;
 
-    @IsString()
-    readonly origem: string;
+    @IsString({message: "morality deve ser string"})
+    @MinLength(2, {message: "morality deve Conter mais de dois caracteres"})
+    @IsNotEmpty({message: "morality não pode estar vazio"})
+    readonly morality: string;
 
-    @IsString()
-    readonly sexo: string;
+
+    @IsString({message: "firstAppearance deve ser string"})
+    @MinLength(2, {message: "firstAppearance deve Conter mais de dois caracteres"})
+    @IsNotEmpty({message: "firstAppearance não pode estar vazio"})
+    readonly firstAppearance:  string;
+
+    @IsDate()
+    @MinDate(new Date("1753-01-01"), {message: "Valor invalido, escolha uma data futura"})
+    @Transform(({ value }) => new Date(value)) 
+    readonly realeseDate: Date;
+
+    
+    @IsString({message: "creator deve ser string"})
+    @MinLength(2, {message: "creator deve Conter mais de dois caracteres"})
+    @IsNotEmpty({message: "creator não pode estar vazio"})
+    readonly creator: string;
+
+    
+    @IsString({message: "weakPoint deve ser string"})
+    @MinLength(2, {message: "weakPoint deve Conter mais de dois caracteres"})
+    @IsNotEmpty({message: "weakPoint não pode estar vazio"})
+    readonly weakPoint: string;
+
+    
+    @IsString({message: "afiliation deve ser string"})
+    @MinLength(2, {message: "afiliation deve Conter mais de dois caracteres"})
+    @IsNotEmpty({message: "afiliation não pode estar vazio"})
+    readonly afiliation: string;
+
+    
+    @IsString({message: "story deve ser string"})
+    @MinLength(2, {message: "story deve Conter mais de dois caracteres"})
+    @IsNotEmpty({message: "story não pode estar vazio"})
+    readonly story: string;
+
+    image?: Express.Multer.File;
+
+    converImage?: Express.Multer.File;
+
 }
