b67c6016614fc963926272ec87932c12c6c9e211
Diego de Souza Lima <diegodesouza.souza@gmail.com>
2025-01-10 21:00:46 -0300

    criacao do fluxo da rota studio, metodos para consumo do front ja finalizados, tratamento de erros pela interface para melhor legitimidade do codigo.

--- src/components/studio/studio.controller.ts
+++ src/components/studio/studio.controller.ts
@@ -1,21 +1,79 @@
-import { Controller, Post, Body, ConflictException } from '@nestjs/common';
+import { Controller, Post, Body, Get, Delete, Param, Put } from '@nestjs/common';
 import { StudioService } from './studio.service';
 import { CreateStudioDto } from './dto/create-studio.dto';
+import {ApiResponse} from './studio.service'
 
 @Controller('studio')
 export class StudioController {
   constructor(private readonly studioService: StudioService) {}
 
   @Post()
-  async registro(@Body() studioDto: CreateStudioDto) {
-    try{
+  async registro(@Body() studioDto: CreateStudioDto): Promise<ApiResponse> {
+    try {
       const result = await this.studioService.create(studioDto);
-      return {result: result, message:'Studio criado com sucesso'};
+      return result;
+    } catch (error) {
+      return {
+        status: 500,
+        message: 'Erro inesperado ao registrar estúdio.',
+        error: error.message || error,
+      };
+    }
+  }
+
+  @Get('find-all-studio')
+  async findAllStudios(): Promise<ApiResponse>{
+    try{
+      const dadosStudios = await this.studioService.findAll();
+      return dadosStudios;
     }catch(error){
-      if(error instanceof ConflictException){
-        throw new ConflictException('Studio com este nome já existe!');
-      }
-      throw error;
+      return {
+        status: 500,
+        message: 'Erro inesperado ao buscar estúdio.',
+        error: error.message || error,
+      };
+    }
+  }
+
+  @Delete('delete-one-studio/:id')
+  async deleteOneStudio(@Param('id') id: number): Promise<ApiResponse>{
+    try{
+      const isDeleted = await this.studioService.DeleteOneStudio(id);
+      return isDeleted;
+    }catch(error){
+      return {
+        status: 500,
+        message: 'Erro inesperado ao deletar estúdio.',
+        error: error.message || error,
+      };
+    }
+  }
+
+  @Get('find-one-studio/:id')
+  async findOneStudio(@Param('id') id: number): Promise<ApiResponse>{
+    try{
+      const findStudio = await this.studioService.findOneStudio(id);
+      return findStudio;
+    }catch(error){
+      return {
+        status: 500,
+        message: 'Erro inesperado ao buscar um estúdio.',
+        error: error.message || error,
+      };
+    }
+  }
+
+  @Put('update/:id')
+  async updateStudio(@Param('id') id: number, @Body('data') data: CreateStudioDto): Promise<ApiResponse>{
+    try{
+      const isUpdate = await this.studioService.UpdateStudio(id, data);
+      return isUpdate;
+    }catch(error){
+      return {
+        status: 500,
+        message: 'Erro inesperado ao atualizar um estúdio.',
+        error: error.message || error,
+      };
     }
   }
 
--- src/components/studio/studio.service.ts
+++ src/components/studio/studio.service.ts
@@ -1,7 +1,15 @@
-import { ConflictException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { CreateStudioDto } from './dto/create-studio.dto';
 import {InjectModel } from '@nestjs/sequelize';
 import { Studio } from '../../models/studio.model';
+
+export interface ApiResponse {
+  status: HttpStatus;
+  message: string;
+  data?: CreateStudioDto[]; 
+  error?: string;
+  dataUnit?: CreateStudioDto;
+}
 
 @Injectable()
 export class StudioService {
@@ -10,20 +18,29 @@ export class StudioService {
       private studioModel: typeof Studio 
     ) {}
   
-  async create(studioDto: CreateStudioDto) : Promise<HttpStatus> {
+  async create(studioDto: CreateStudioDto) : Promise<ApiResponse> {
     try{
       const existingStudio = await this.studioModel.findOne({where: { name: studioDto.name}
       });
-
       if(existingStudio){
-        throw new ConflictException('Já existe um registro n tabela equipes com este nome.')
+        return {
+          status: HttpStatus.CONFLICT,
+          message:'Já existe um registro n tabela equipes com este nome.'
+        }
       }
 
       await this.studioModel.create(studioDto);
 
-      return HttpStatus.CREATED;
+      return {
+        status: HttpStatus.CREATED,
+        message: 'Registro criado com sucesso!'
+      };
     }catch(error){
-
+      console.error("Erro no método create:", error);
+      return {
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: `Erro ao tentar registrar um novo studio: ${error.message || error}`
+      };
     }
   }
 
@@ -31,4 +48,99 @@ export class StudioService {
     const studio = await this.studioModel.findOne({where: {id}});
     return studio != null;
   }  
+
+  async findAll(): Promise<ApiResponse>{
+    try{
+      const dadosStudios = await this.studioModel.findAll({attributes: ['id','name', 'nationality']});
+      if (dadosStudios.length === 0){
+        return {
+          status: HttpStatus.NO_CONTENT,
+          message: 'Nenhum estúdio encontrado.'
+        };
+      }
+      return {
+        status: HttpStatus.OK,
+        message: 'Estúdios encontrados com sucesso.',
+        data: dadosStudios
+      };
+    }catch(error){
+      console.error(error);
+      return {
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: `Erro ao recuperar os estúdios: ${error.message || error}`
+      };
+    }
+  }
+
+  async DeleteOneStudio(id: number): Promise<ApiResponse>{
+    try{
+      const isDeleted = await this.studioModel.destroy({where:{id: id}});
+      if(isDeleted > 0){
+        return {
+          status: HttpStatus.OK,
+          message: 'Studio deletado com sucesso!'
+        };
+      }
+      
+      return {
+        status: HttpStatus.NOT_FOUND,
+        message: 'Studio não encontrado.'
+      }
+    }catch(error){
+      console.error(error);
+      return {
+        status:HttpStatus.INTERNAL_SERVER_ERROR,
+        message: `Erro ao tentar apagar registro do studio: ${error.message || error}`
+      } ;
+    }
+  }
+
+  async findOneStudio(id: number): Promise<ApiResponse>{
+    try{
+      const isStudio = await this.studioModel.findOne({where:{id}});
+      if(!isStudio){
+        return {
+          status: HttpStatus.CONFLICT,
+          message: 'Não foi possivel encontrar o registro desejado.'
+        }
+      }
+
+      return {
+        status:HttpStatus.OK,
+        message: 'Registro encontrado com sucesso!',
+        dataUnit: isStudio
+      }
+    }catch(error){
+      console.error(error);
+      return {
+        status:HttpStatus.INTERNAL_SERVER_ERROR,
+        message: `Erro ao tentar apagar registro do studio: ${error.message || error}`
+      } ;
+    }
+  }
+
+  async UpdateStudio(id: number, studioDto: CreateStudioDto): Promise<ApiResponse>{
+    try{
+      const [affectedRows] = await this.studioModel.update(studioDto, {
+        where: { id: id }
+      });
+      if (affectedRows === 0) {
+        return {
+          status: HttpStatus.NOT_FOUND,
+          message: 'Estúdio não encontrado.',
+        };
+      }
+  
+      return {
+        status: HttpStatus.OK,
+        message: 'Estúdio atualizado com sucesso!',
+      };
+    }catch(error){
+      console.error(error);
+      return {
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: `Erro ao tentar atualizar o estúdio: ${error.message || error}`,
+      };
+    }
+  }
 }
